05dad99aae2e9139ed73f87914bc0cc95600a0e4
Shweta Chaudhary <shweta.chaudhary@kellton.org>
2024-05-17 12:06:28 +0530

    new

---
+++ JS_Info/object.js
@@ -0,0 +1,33 @@
+// const prompt = require("")
+// // let obj = 
+// // {
+// // name:"shweta",
+// // class:10,
+// // }
+
+// // obj.thing = "anything";
+// // obj["shweta chaudhary"] ="Employee"
+// // console.log(obj)
+
+// // let key = "name";
+// // console.log( obj.key )
+// // // console.log(obj.name)
+// // // console.log(obj.class)
+// // delete obj.class;
+// // console.log(obj)
+
+
+// //Computed properties
+// let fruit = prompt("Which fruit to buy", " apple")
+// let bag = {
+//     [fruit ] : 5
+// }
+// console.log(bag.fruit)
+
+const xyz = '767575756'
+console.log(xyz)
+
+
+
+let a = 10_00
+console.log(a)
